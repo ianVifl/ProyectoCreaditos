cf7107e0f66dbf806cfe7a34a1a46119a517807a
ianVifl <ianvillamil06@gmail.com>
2025-05-12 09:38:02 -0600

    Proyecto Creditos

---
+++ src/main/java/com/mycompany/creditosuaem/CreditosUAEM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.creditosuaem;
+
+/**
+ *
+ * @author ianvi
+ */
+public class CreditosUAEM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
